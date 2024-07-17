2e3452ee28ceb7670d214e65b0780735d9d05bf3
Igor <igoralbuquerque021@gmail.com>
2024-07-17 19:57:50 -0300

    chore: creation of dockerfile and compose of system

--- payment-microservice-consumer/src/main/java/com/studies/payment_microservice_consumer/model/Payment.java
+++ payment-microservice-consumer/src/main/java/com/studies/payment_microservice_consumer/model/Payment.java
@@ -3,7 +3,9 @@ package com.studies.payment_microservice_consumer.model;
 import java.io.Serializable;
 
 import lombok.Getter;
+import lombok.ToString;
 
+@ToString
 @Getter
 public class Payment implements Serializable {
 	private Long id;
--- payment-microservice-producer/src/main/java/com/studies/payment_microservice/service/impl/PaymentServiceImpl.java
+++ payment-microservice-producer/src/main/java/com/studies/payment_microservice/service/impl/PaymentServiceImpl.java
@@ -3,6 +3,7 @@ package com.studies.payment_microservice.service.impl;
 import java.io.Serializable;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
 import com.studies.payment_microservice.model.Payment;
 import com.studies.payment_microservice.service.PaymentService;
@@ -13,6 +14,7 @@ import lombok.extern.log4j.Log4j2;
 
 @RequiredArgsConstructor
 @Log4j2
+@Service
 public class PaymentServiceImpl implements PaymentService {
 
 	private final KafkaTemplate<String, Serializable> kafkaTemplate;
